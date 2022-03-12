8b7b366bfdf158804fc914b2a6a5e657f89001b0
Baokker <1048098862@qq.com>
2022-03-12 17:16:45 +0800

    forget to add

--- voiceprint/main.m
+++ voiceprint/main.m
@@ -1,4 +1,4 @@
-% Train('./SpeakerVerificationLock/CNC2/*/*.wav','./model/BaseModel.mat')
+Train('./SpeakerVerificationLock/CNC2/*/*.wav','./model/BaseModel.mat')
 % move file
 Enroll('./SpeakerVerificationLock/Enroll/*/*.wav','./SpeakerVerificationLock/Det/','./model/BaseModel.mat','./model/CheckPoint.mat','./model/errThreshold.mat')
 Verify('./SpeakerVerificationLock/Test/authenticate_audio.wav','./model/CheckPoint.mat','./model/errThreshold.mat','fbc')
